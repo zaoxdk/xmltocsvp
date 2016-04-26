a3c6ddcbab4beb1e3074f34d8777164fc413db35
zaoxdk <zaox_dk@hotmail.dk>
2016-04-26 15:39:00 +0200

    Xml To Csv
    This is the converter

---
+++ xmltocsv.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class xmltocsv
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OFD = New System.Windows.Forms.OpenFileDialog()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SFD = New System.Windows.Forms.SaveFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "XML:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "CSV output:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(292, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Open file"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(292, 100)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Select"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OFD
+        '
+        Me.OFD.Filter = "XML log file|*.xml"
+        Me.OFD.InitialDirectory = "/C:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(88, 44)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(183, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(88, 100)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(183, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 217)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(355, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "This progam handles the conversion of the output file from Nefmoto logger"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 239)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(141, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "from the XML format to CSV."
+        '
+        'SFD
+        '
+        Me.SFD.DefaultExt = "csv"
+        Me.SFD.Filter = "CSV log file|*.csv"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(292, 160)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Convert"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(21, 160)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Reset"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(169, 138)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Status"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(136, 161)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(104, 18)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Converting..."
+        '
+        'xmltocsv
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(379, 267)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.HelpButton = True
+        Me.MaximizeBox = False
+        Me.Name = "xmltocsv"
+        Me.Text = "XML to CSV Converter"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OFD As OpenFileDialog
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents SFD As SaveFileDialog
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ xmltocsv.vb
@@ -0,0 +1,213 @@
+﻿#Region "Imports"
+Imports System.Xml
+Imports System.IO
+Imports System.Xml.Xsl
+Imports System.Xml.XPath
+Imports Microsoft.VisualBasic
+#End Region
+
+
+
+
+
+'Hello World - XML to CSV converter, used for converting the output xml file from Nefmoto flasher/logger logging program, into a CSV file, which is 
+'readable to EcuxPlot. Only for educational and hobby use! - By Nefmoto user ZaoxDK! 
+'Project start: 22-04-2016
+'Projext end: 
+
+
+
+'**********************Project Notes*****************************
+'File Called work.xml is the file, which we use to decrypt and read data from, so that the original xml file is untouched, as it simply gets copied into work.xml.
+'Therefore we also need a work_done.xml file, which will be the file, that we write the decrypted data to. This is then the file, that gets converted into CSV.
+
+'***************WORKAROUND***************************
+
+'1. Load user selected file via OFD
+'2. User selects destination file via SFD
+'3. User presses convert - code @Button_3 initiates
+'1a. Copy original xml to work.xml
+'2a. Read from encrypted work.xml **NOT DONE**
+'3a. Decrypt the RawData from nefmoto log xml**NOT DONE**
+'4a. Either overwrite decrypted data or write to new xml called work_done.xml (if write to new) **NOT DONE**
+'5a. Convert work.xml or work_done.xml to CSV using correct xsl stylesheet. **NOT DONE**
+'6a. Delete all work.xml files...
+'----------End of code----------
+'****************************************************
+
+'*************RESOURCES*********************
+' Some of the code used, has been copied directly from Microsoft (https://msdn.microsoft.com)
+'
+'
+'
+'
+'
+'*******************Updates**************************
+' By 25-04-2016: Loading and saving files now works, also converting to CSV using a XSL stylesheet, which is defined in the code
+' TODO: Alot... Mainly to get the decode function working, but also to create a stylesheet which matches the layout of the sample log.csv file
+
+
+
+
+
+Public Class xmltocsv
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Label6.Visible = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (OFD.ShowDialog() = DialogResult.OK) Then
+            TextBox1.Text = System.IO.Path.GetFileName(OFD.FileName)
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If (SFD.ShowDialog() = DialogResult.OK) Then
+            TextBox2.Text = System.IO.Path.GetFileName(SFD.FileName)
+        End If
+
+
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        If TextBox1.Text = "" Then
+            MsgBox("Please select a log file..", MsgBoxStyle.Critical, "Error")
+            GoTo Over
+
+
+        End If
+
+        If TextBox2.Text = "" Then
+            MsgBox("Please save an output file..", MsgBoxStyle.Critical, "Error")
+            GoTo Over
+        End If
+
+        MsgBox("Press OK to convert", MsgBoxStyle.Information, "Conversion")
+
+        'Converting label turns on
+        Label6.Visible = True
+
+        'check If file Is existing
+        If (IO.File.Exists(OFD.FileName)) Then
+
+            'Copy the selected file to the work.xml file
+            My.Computer.FileSystem.CopyFile(
+            OFD.FileName,
+            "work.xml",
+            Microsoft.VisualBasic.FileIO.UIOption.AllDialogs,
+            Microsoft.VisualBasic.FileIO.UICancelOption.DoNothing)
+
+
+            '***************Data Fetch + delete Section*************************************************
+
+
+
+            Dim doc As XmlDocument = New XmlDocument()
+            doc.Load("work.xml")
+
+            'Remove all attributes of the description node - this fetches all nodes called by the end tag */(endtag) in this case: "description"
+            Dim nodeList As XmlNodeList
+            Dim root As XmlElement = doc.DocumentElement
+            nodeList = root.SelectNodes("/LogFile/VariableDefinitions/MemoryVariableDefinition/Description")
+            ' Here the function does something, this can be altered to ex. convert base64 data.
+            Dim Description As XmlNode
+            For Each Description In nodeList
+                Description.RemoveAll()
+            Next
+
+
+            Dim nodeList2 As XmlNodeList
+            Dim root2 As XmlElement = doc.DocumentElement
+            nodeList2 = root.SelectNodes("/LogFile/VariableDefinitions/MemoryVariableDefinition/ScaleOffsetMemoryVariableValueConverter")
+            Dim DSOMVVC As XmlNode
+            For Each DSOMVVC In nodeList2
+                DSOMVVC.RemoveAll()
+            Next
+
+            Dim nodeList3 As XmlNodeList
+            Dim root3 As XmlElement = doc.DocumentElement
+            nodeList3 = root.SelectNodes("/LogFile/VariableDefinitions/MemoryVariableDefinition/Address")
+            Dim address As XmlNode
+            For Each address In nodeList3
+                address.RemoveAll()
+            Next
+
+            Dim nodeList4 As XmlNodeList
+            Dim root4 As XmlElement = doc.DocumentElement
+            nodeList4 = root.SelectNodes("/LogFile/VariableDefinitions/MemoryVariableDefinition/DataType")
+            Dim DT As XmlNode
+            For Each DT In nodeList4
+                DT.RemoveAll()
+            Next
+
+
+
+
+
+
+
+
+
+
+
+            'Save the modified xml
+            doc.Save("work_mod.xml")
+
+
+
+
+
+
+
+
+
+
+            '*****************************Decryption section*********************************************
+
+
+
+            ' Initialize an XslCompiledTransform instance.
+            Dim transform As New XslCompiledTransform()
+
+            'Load the Stylesheet ***Place correct Stylesheet***
+            transform.Load("Books.xslt")
+
+            'Transform the loaded xml to csv using the specified stylesheet
+            transform.Transform("work_mod.xml", SFD.FileName)
+
+
+
+        End If
+        If (IO.File.Exists(SFD.FileName)) Then
+            'This is only noted for developer reasons
+            My.Computer.FileSystem.DeleteFile("work.xml")
+            'Converting label turns off
+            Label6.Visible = False
+            GoTo Finished
+
+        End If
+
+EarlyExit:
+
+        MsgBox("Something went wrong, please try again And make sure that selected file Is a Nefmoto Logfile..", MsgBoxStyle.Critical, "Error")
+        GoTo Over
+
+Finished:
+        MsgBox("File correctly converted. Find it at specified location 
+        Thanks for using this software - Enjoy", MsgBoxStyle.Information, "Succes!")
+        GoTo Over
+Over:
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Controls.Clear()
+        InitializeComponent()
+    End Sub
+End Class
